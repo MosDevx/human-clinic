ded23a026d07d6cd458171e24a02934f248c0268
MosDevx <mosesk.wamae@gmail.com>
2023-02-08 12:26:53 +0300

    Revert "Edit schema; add TABLES patients, medical_histories, treatments" Revert push This reverts commit 551b4e3a52c2e32193c11eea7d308e771ae4f900.

--- schema.sql
+++ schema.sql
@@ -1,7 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE patients(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name VARCHAR(100) NOT NULL, date_of_birth DATE);
-
-CREATE TABLE medical_histories (id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, admitted_at TIMESTAMP, patient_id INT, status VARCHAR(50) , CONSTRAINT fk_patient_id FOREIGN KEY(patient_id) REFERENCES patients(id) ON DELETE CASCADE );
-
-CREATE TABLE treatments (id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, type VARCHAR(200) NOT NULL,name VARCHAR(200) NOT NULL);
+CREATE TABLE animals (
+    name varchar(100)
+);
